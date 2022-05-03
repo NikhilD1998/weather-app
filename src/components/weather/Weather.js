c2e95beee82702142a3a084a9fd896601aa7195c
NikhilD1998 <nikhild.dev.1998@gmail.com>
2022-05-03 18:07:33 +0530

    Fixed weatherdata not rendering issue

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,19 +1,23 @@
+import { useEffect, useState } from "react";
+
 const Weather = () => {
-  navigator.geolocation.getCurrentPosition(function (position) {
-    var lat = position.coords.latitude;
-    var long = position.coords.longitude;
-    var apiKey = "72fa26a26e718e29a8d690026ff2dd93";
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
-    const weatherData = fetch(url)
-      .then((res) => res.json())
-      .then((result) => {
-        const coord = result.weather[0].description;
-        console.log(coord);
-      });
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      var apiKey = "72fa26a26e718e29a8d690026ff2dd93";
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
+      fetch(url).then((res) => res.json()).then((result) => {
+          setDescription(result.weather[0].description);
+        });
+    });
   });
+
   return (
     <div>
-      <h1>Test</h1>
+      <h1>{description}</h1>
     </div>
   );
 };
