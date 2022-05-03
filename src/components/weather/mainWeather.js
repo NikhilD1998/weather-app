d539914b770d6847293f01892d5828769c422d04
NikhilD1998 <nikhild.dev.1998@gmail.com>
2022-05-03 17:16:14 +0530

    Main

---
+++ src/components/weather/mainWeather.js
@@ -0,0 +1,23 @@
+
+const fetchdata = () => {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    var apiKey = "72fa26a26e718e29a8d690026ff2dd93";
+    var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`
+    var weatherData = fetch(url).then(res => res.json()).then(result => {
+    var coord = result.weather[0].description
+    })
+  });
+}
+
+
+const Weather = () => {
+ return(
+   <div>
+     <h1></h1>
+   </div>
+ )
+}
+
+export default Weather
