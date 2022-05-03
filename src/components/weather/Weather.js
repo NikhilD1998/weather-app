264e15223156eaf7958a209d14cdcfa30fee3f1d
NikhilD1998 <nikhild.dev.1998@gmail.com>
2022-05-03 17:28:14 +0530

    main

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -3,17 +3,19 @@ const Weather = () => {
     var lat = position.coords.latitude;
     var long = position.coords.longitude;
     var apiKey = "72fa26a26e718e29a8d690026ff2dd93";
-    var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`
-    var weatherData = fetch(url).then(res => res.json()).then(result => {
-    var coord = result.weather[0].description
-    console.log(coord);
-    })
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
+    const weatherData = fetch(url)
+      .then((res) => res.json())
+      .then((result) => {
+        const coord = result.weather[0].description;
+        console.log(coord);
+      });
   });
-  return(
+  return (
     <div>
-      <h1></h1>
+      <h1>Test</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Weather
+export default Weather;
